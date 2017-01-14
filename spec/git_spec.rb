a6d908410abb837930c7a464784e5bc8bed59ea7
Harsh Lathwal <xeon.harsh@gmail.com>
2017-01-14 20:26:00 +0530

    reboot

--- spec/git_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'Fun' do
-
-  context 'undo git add' do
-    it { responds_with say: "To undo a single file use\n\ngit reset filespec\n\n\nTo undo ALL files added (i.e. you want to undo git add .) then use\n\ngit reset" }
-  end
-
-end
--- spec/weather_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe "Weather" do
-
-  context "weather like in San Francisco" do
-    it { responds_with command: "curl -sS wttr.in/San%20Francisco", :explanation => "Show weather for given place." }
-  end
-
-end
