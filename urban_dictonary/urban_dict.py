50a0ec13f54ed5ccfe23505c9ff698dd6682fbea
RenanLeviathan <renan.leviathan@gmail.com>
2017-10-01 19:29:42 -0300

    Revert "bug fix #1"
    This reverts commit 8892a454e5648207a1bcff7907e2b52042767941.

--- urban_dictonary/urban_dict.py
+++ urban_dictonary/urban_dict.py
@@ -1,7 +1,6 @@
-#-*- coding: utf-8-*-
 import csv
 def urban_dict(word):
-	with open('urban_dictonary/urban_dictionary.csv', 'rt', encoding="utf8") as f:
+	with open('urban_dictonary/urban_dictionary.csv', 'rt') as f:
 		reader = csv.reader(f, delimiter=',')
 		for row in reader:
 			if word == row[1]:#what to do with dublicates ? remove them , how ?
