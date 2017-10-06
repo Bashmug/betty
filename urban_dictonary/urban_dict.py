5b034e368a7fb88ccd069c212050537ff4411239
RenanLeviathan <renan.leviathan@gmail.com>
2017-10-06 10:59:17 -0300

    Feature entries with (#33)
    * changes in readme (running betty in Windows)
    
    * bug fix #1
    
    the betty module didn't load the csv file properly (Unicode error message appeared)
    
    * Help list
    
    * help list
    
    When users type betty in command prompt, betty will show its useful commands
    
    * help list
    
    adding the help list, which appears when betty runs with no arguments
    
    * Create .gitignore
    
    * Update .gitignore
    
    * search by parts of the words added
    
    It is useful for who wants to search for a word, but doesn't know it completely
    
    * gitignore settings
    
    * gitignore
    
    * gitignore settings #2
    
    * Delete __init__.cpython-36.pyc
    
    * Delete urban_dict.cpython-36.pyc
    
    * fixed gitignore #3

--- urban_dictonary/urban_dict.py
+++ urban_dictonary/urban_dict.py
@@ -1,9 +1,19 @@
+#-*- coding: utf-8-*-
 import csv
 def urban_dict(word):
-	with open('urban_dictonary/urban_dictionary.csv', 'rt') as f:
+	with open('urban_dictonary/urban_dictionary.csv', 'rt', encoding="utf8") as f:
 		reader = csv.reader(f, delimiter=',')
 		for row in reader:
-			if word == row[1]:#what to do with dublicates ? remove them , how ?
+			if word == row[1]:#what to do with duplicates ? remove them , how ?
 				print (row[0])
+		else:
+			return 0
+
+def entries_with(subs):
+	with open('urban_dictonary/urban_dictionary.csv', 'rt', encoding="utf8") as f:
+		reader = csv.reader(f, delimiter=',')
+		for row in reader:
+			if row[1].find(subs) != -1:#searching for an entry with the substring within it
+				print ("Word:",row[1],"\nDefinition:",row[0],"\n\n")
 		else:
 			return 0
